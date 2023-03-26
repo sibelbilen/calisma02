ecd5afa7941e36b95811c53b10a119cb81f37f9b
ERELOZTURK <erelozturk2@gmail.com>
2023-03-26 21:53:30 +0300

    v7

--- src/main/java/C01.java
+++ src/main/java/C01.java
@@ -1,5 +1,7 @@
 public class C01 {
     public static void main(String[] args) {
         System.out.println("sibel");
+        System.out.println("Derse 10 dk kaldı");
+        System.out.println("Ali Hocam hayat kurtardın");
     }
 }
